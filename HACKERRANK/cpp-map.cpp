9b9837ec35613cb5c97d48d774576a08749b5e75
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2021-10-07 20:51:57 +0600

    07.10.2021

---
+++ HACKERRANK/cpp-map.cpp
@@ -0,0 +1,39 @@
+///////////// IIUC - 49 /////////////
+#include<bits/stdc++.h>
+using namespace std;
+#define ll              long long
+
+#define all(n)          n.begin(), n.end()
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll n;
+    cin >> n;
+    map<string , ll>mp;
+    while(n--){
+        ll q;
+        cin >> q;
+        if (q == 1){
+            string s;
+            ll m;
+            cin >> s >> m;
+            mp[s] += m;
+        }
+        if (q == 2){
+            string s;
+            cin >> s;
+            // for(auto x : mp) cout << x.first << " " << x.second << endl;
+            mp.erase(s);
+        }
+        if(q == 3){
+            string s;
+            cin >> s;
+            if(mp.find(s) == mp.end()) cout << 0 << endl;
+            else cout <<  mp[s] << endl;
+        }
+    }
+
+    return 0;
+
+}
---
+++ UVA/UVA-401.cpp
@@ -0,0 +1,90 @@
+//       || اقْرَأْ بِاسْمِ رَبِّكَ الَّذِى خَلَقَ|| بسم الله الرحمن الرحيم  
+#include<bits/stdc++.h>
+using namespace std;
+#define ll              long long
+#define MX              1000000007
+#define db(x)           cout << #x << " : " << x << endl;
+#define faster          ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pera            freopen("inputf.in","r", stdin);freopen("outputf.in","w", stdout)
+#define test            ll t; cin >> t; while(t--){solve();};
+#define py              cout << "YES\n"
+#define pn              cout << "NO\n"
+#define all(n)          n.begin(), n.end()
+#define uper(n)         transform(n.begin(), n.end(), n.begin(), ::toupper);
+#define lower(n)        transform(n.begin(), n.end(), n.begin(), ::tolower);
+#define pb              push_back
+#define sz(n)           (int)n.size()
+#define ff              first
+#define ss              second
+#define en              "\n"
+
+ll arr[256]={};
+string f1 = "AEHIJLMOSTUVWXYZ12358";
+string f2 = "A3HILJMO2TUVWXY51SEZ8";
+/* Start Your CODE*/
+
+
+void solve()
+{
+    string s; 
+    for(ll i=0; i<sz(f1); i++) arr[f1[i]]=i+1;
+    while(cin >> s && s != ""){
+        string s1, s2;
+        s1 = s2 = s;
+        ll f = 0;
+        reverse(all(s1));
+        if (s1 == s2){  
+            f = 0;
+            for(ll i=0; i<sz(s)/2+1; i++){
+                ll r = arr[s[i]] - 1;
+
+                if(f2[r] == s1[i]){
+                    // db(f1[r]);
+                     // cout << arr[s[i]] << endl;
+                    continue;
+                } 
+                else {
+                    f = 1;
+                    break;
+                }
+            }
+
+            if (f) cout << s << " -- is a regular palindrome.\n\n";
+            else {
+                cout << s << " -- is a mirrored palindrome.\n\n";
+            }
+        }
+        else{
+            f = 0;
+            for(ll i=0; i<sz(s); i++){
+                ll r = arr[s[i]]-1 ;
+                if (f2[r] == s[sz(s) - 1 - i]);
+                else {
+                    f = 1; break;
+                }
+            }
+            if (f == 1) cout << s << " -- is not a palindrome.\n\n";
+            else cout << s << " -- is a mirrored string.\n\n";
+        }
+    }
+}
+/* Start Your FUN*/
+ll fun(ll n)
+{
+    
+}
+/*   copy input / copy output
+
+
+*/
+/*  Start Your MAIN*/
+int main()
+{
+    faster
+    solve();
+    /*
+    test
+    fun();
+    */
+    return 0;
+}
