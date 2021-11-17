2ca1019ab2d44fa355961db4a421c57d4c6e7afe
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2021-11-17 23:21:55 +0600

    17.11.2021

---
+++ UVA/UVA 10405.cpp
@@ -0,0 +1,73 @@
+//       || اقْرَأْ بِاسْمِ رَبِّكَ الَّذِى خَلَقَ|| بسم الله الرحمن الرحيم  
+#include<bits/stdc++.h>
+using namespace std;
+#define ll              long long
+#define MX              10000
+#define db(x)           cout << #x << " : " << x << endl;
+#define faster          ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pera            freopen("inputf.in","r", stdin);freopen("outputf.in","w", stdout)
+#define test            ll t; cin >> t; while(t--){solve();};
+#define py              cout << "YES\n"
+#define pn              cout << "NO\n"
+#define all(n)          n.begin(), n.end()
+#define uper(n)         transform(n.begin(), n.end(), n.begin(), ::toupper);
+#define lower(n)        transform(n.begin(), n.end(), n.begin(), ::tolower);
+#define pb              push_back
+#define sz(n)           n.size()
+#define ff              first
+#define ss              second
+#define en              "\n"
+ll Pow(ll b, ll p){return p==0? 1:b*Pow(b, p-1);}
+ll gcd(ll a, ll b) {return(b==0)?a:gcd(b, a%b);}
+ll lcm(ll a, ll b) {return (a / gcd(a, b)) * b;}
+string to_String(int n){stringstream ss;string s;ss<<n;ss>>s;cout<<s<<endl;}
+ll to_int(string s){ll n;stringstream ss(s); ss>>n;cout << n << endl;}
+ll BigMod(ll b, ll p, ll m) {if (p == 0) return 1;ll ans = BigMod(b, p/2, m);ans = ( ans * ans ) % m;if (p % 2 == 1) ans = (ans * b) % m;return ans;}
+ll binarySearch(ll arr[], ll l, ll r, ll x) {if (r >= l){ll mid = l+(r-l) / 2;if (arr[mid] == x) return mid;if (arr[mid] > x) return binarySearch(arr, l, mid-1, x);return binarySearch(arr, mid+1, r, x);}return -1 ;}
+ll ModInv(ll a, ll m){return BigMod(a, m-2, m);}
+ll egcd(ll a, ll b, ll &x, ll &y){if(b==0){x=1;y=0;return a;}ll x1,y1;ll d=egcd(b,a%b,x1,y1);x=y1;y=x1-(a/b)*y1;return d;}
+ll fact(ll n){if(n==1)return 1;else return (n * fact(n-1));}
+void sieve (vector<ll>&v){v.assign(MX, 0);v[0] = v[1] = 1;for (int i=4; i<=MX; i+=2) v[i] = 1;for (int i=3; i*i<=MX; i+=2){if (v[i] == 0){for (int j=i*i; j<=MX; j+= i*2)v[j] = 1;}}}
+
+/*
+    PROBLEM START : Wed Nov 17 22:54:28 2021
+    PROBLEM END :   Wed Nov 17 23:19:33 2021
+    PROBLEM LINK : https://onlinejudge.org/external/100/10066.pdf
+*/
+
+ll n, m;    
+std::vector<ll>sec(MX);
+std::vector<ll>tow(MX);
+ll LC[MX][MX];
+string a, b;
+ll LCS(){
+    memset(LC, 0, sizeof(LC));
+
+    for(ll i=1; i<=n; i++){
+        for(ll j=1; j<=m; j++){
+            if (a[i-1] == b[j-1]) LC[i][j] = LC[i -1][j -1]+1;
+            else LC[i][j] = max(LC[i][j-1],LC[i-1][j]);
+        }
+    }
+    return LC[n][m] ;
+}
+/* Start Your CODE*/
+void solve()
+{
+    while(getline(cin, a)){
+        getline(cin , b);
+        n = sz(a); m = sz(b);
+        cout << LCS() << endl;
+    }
+}
+int main()
+{
+    // faster
+    solve();
+    /*
+    test
+    fun();
+    */
+    cerr << "Time takeen : " << (float)clock()/CLOCKS_PER_SEC << " secs\n";time_t ttime = time(0); char* dt = ctime(&ttime);cerr << dt << endl;
+    return 0;
+}
