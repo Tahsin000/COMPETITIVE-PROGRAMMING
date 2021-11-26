918e4f6b8de201c3ef0ad979ddd869a0283d1884
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2021-11-27 00:48:16 +0600

    26.11.2021

--- JAMIL_SIR/CLASS-15 (DP) [ 26.11.20211 ].cpp
+++ JAMIL_SIR/CLASS-15 (DP) [ 26.11.20211 ].cpp
@@ -13,6 +13,11 @@ using namespace std;
     1. memoizition 
     2. overlapping subproblems 
     3. optimal substructure 
+**** HOME_WORK ****
+    https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/V
+    https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/X
+    https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/Y
+    https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/Z
 */
 
 ll dp[35][35];
